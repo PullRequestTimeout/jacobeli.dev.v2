b8867fad6c8bd353b31f6f7c666ee4a1213d1227
Jacob Druery <95809005+PullRequestTimeout@users.noreply.github.com>
2023-09-20 15:55:03 -0700

    :art: Prettier auto format

--- src/lib/components/Projects.svelte
+++ src/lib/components/Projects.svelte
@@ -1,37 +1,37 @@
 <script>
-    import ProjectCard from "./ProjectCard.svelte"
-    import Title from "./Title.svelte"
-    import { projects } from "../data/projects"
-    let openedCards = Array(projects.length).fill(false)
+	import ProjectCard from "./ProjectCard.svelte";
+	import Title from "./Title.svelte";
+	import { projects } from "../data/projects";
+	let openedCards = Array(projects.length).fill(false);
 
-    function handleCloseCards(index, isOpened) {
-        openedCards = openedCards.map((_, i) => (i === index ? isOpened : false))
-    }
+	function handleCloseCards(index, isOpened) {
+		openedCards = openedCards.map((_, i) => (i === index ? isOpened : false));
+	}
 </script>
 
 <section>
-    <Title innerText="Selected Works" id="work" />
-    {#each projects as project, index}
-        <hr />
-        <ProjectCard
-            title={project.title}
-            subtitle={project.subtitle}
-            date={project.date}
-            description={project.description}
-            video={project.video}
-            image={project.image}
-            link={project.link}
-            checked={openedCards[index]}
-            on:closeCards={(event) => handleCloseCards(index, event.detail)}
-        />
-    {/each}
-    <hr />
+	<Title innerText="Selected Works" id="work" />
+	{#each projects as project, index}
+		<hr />
+		<ProjectCard
+			title={project.title}
+			subtitle={project.subtitle}
+			date={project.date}
+			description={project.description}
+			video={project.video}
+			image={project.image}
+			link={project.link}
+			checked={openedCards[index]}
+			on:closeCards={(event) => handleCloseCards(index, event.detail)}
+		/>
+	{/each}
+	<hr />
 </section>
 
 <style>
-    hr {
-        width: 100%;
-        border: none;
-        border-bottom: 1px solid var(--clr-white);
-    }
+	hr {
+		width: 100%;
+		border: none;
+		border-bottom: 1px solid var(--clr-white);
+	}
 </style>
