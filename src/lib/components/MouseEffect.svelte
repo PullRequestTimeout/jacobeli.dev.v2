d8be8e3e53336802b0fda3d652dba8f5abd47df0
Jacob Druery <95809005+PullRequestTimeout@users.noreply.github.com>
2023-10-02 20:29:19 -0700

    :zap: Changed css mouse effect to png

--- src/lib/components/MouseEffect.svelte
+++ src/lib/components/MouseEffect.svelte
@@ -1,12 +1,12 @@
 <script>
-	import { onMount } from 'svelte';
+	import { onMount } from "svelte";
 
 	let blob;
 
 	onMount(() => {
-		document.addEventListener('pointermove', handlePointerMove);
+		document.addEventListener("pointermove", handlePointerMove);
 		return () => {
-			document.removeEventListener('pointermove', handlePointerMove);
+			document.removeEventListener("pointermove", handlePointerMove);
 		};
 	});
 
@@ -14,18 +14,25 @@
 		const { clientX, clientY } = event;
 		blob.animate(
 			{ left: `${clientX}px`, top: `${clientY}px` },
-			{ duration: 5000, fill: 'forwards' }
+			{ duration: 5000, fill: "forwards" }
 		);
 	}
 </script>
 
 <div class="container">
-	<div role="none" on:mousemove={handlePointerMove} bind:this={blob} class="mouse-effect" />
-	<div class="blur" />
+	<img
+		class="mouse"
+		on:mousemove={handlePointerMove}
+		bind:this={blob}
+		role="presentation"
+		src="/assets/glow.png"
+		alt="Glow effect that tracks cursor"
+	/>
 </div>
 
 <style>
 	.container {
+		pointer-events: none;
 		width: 100%;
 		height: 100%;
 		position: fixed;
@@ -34,57 +41,19 @@
 		left: 0;
 		overflow: hidden;
 	}
-
-	.mouse-effect {
+	.mouse {
+		height: 50rem;
 		display: none;
 		top: 50%;
 		left: 50%;
-		position: absolute;
 		transform: translate(-50%, -50%);
-		background-color: rgba(161, 121, 69, 0.5);
-		border-radius: 50%;
-		animation: blob 30s infinite linear;
-		z-index: -100;
-		aspect-ratio: 1.5;
-		/* Man this is probably really computationally expensive to have a filter and backdrop filter, but bd-filter is buggy as hell */
-		filter: blur(4rem);
+		position: absolute;
+		z-index: -99;
+		opacity: 0.2;
 	}
 
-	.blur {
-		display: none;
-		top: 0;
-		left: 0;
-		height: 100%;
-		width: 100%;
-		position: fixed;
-		z-index: -50;
-		backdrop-filter: blur(5rem);
-	}
-
-	@keyframes blob {
-		0% {
-			/* aspect-ratio: 1; */
-			height: 25rem;
-			transform: translate(-50%, -50%) scale(1) rotate(0deg);
-		}
-		50% {
-			/* aspect-ratio: 1.5; */
-			height: 15rem;
-			transform: translate(-50%, -50%) scale(1.1) rotate(180deg);
-		}
-		100% {
-			/* aspect-ratio: 1; */
-			height: 25rem;
-			transform: translate(-50%, -50%) scale(1) rotate(360deg);
-		}
-	}
-
-	@media screen and (min-width: 1280px) {
-		.mouse-effect {
-			display: unset;
-		}
-
-		.blur {
+	@media screen and (min-width: 1024px) {
+		.mouse {
 			display: unset;
 		}
 	}
