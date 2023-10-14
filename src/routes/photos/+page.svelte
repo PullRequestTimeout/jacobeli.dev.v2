077d4def472aa3ff76b7479b0bd45017d4a59e99
Jacob Druery <95809005+PullRequestTimeout@users.noreply.github.com>
2023-10-14 16:00:56 -0700

    :children_crossing: Update title on route

--- src/routes/photos/+page.svelte
+++ src/routes/photos/+page.svelte
@@ -3,5 +3,9 @@
 	import PhotoHero from "../treehugger/PhotoHero.svelte";
 </script>
 
+<svelte:head>
+	<title>Photos - Jacob Druery // Creative Developer</title>
+</svelte:head>
+
 <PhotoHero subtitle="// Sometimes I take photos" />
 <Carousel />
