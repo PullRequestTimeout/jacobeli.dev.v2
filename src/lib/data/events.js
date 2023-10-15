f97fe988e2e872a7b6f53847c490c0665e1af9a9
Jacob Druery <95809005+PullRequestTimeout@users.noreply.github.com>
2023-10-14 19:40:20 -0700

    :construction: Added events card to photos

---
+++ src/lib/data/events.js
@@ -0,0 +1,12 @@
+export const events = [
+	{
+		title: "TreeHugger Hare Scramble",
+		subtitle: "Enduro event - Kootenay Motorcross Track",
+		date: "2023",
+		description:
+			"This hare scramble style enduro event organised by Daniel Kuin and a team of volunteers on behalf of Kootenay Motorcross Track took place on the 16th and 17th of September. Check out the event page to see the photos I shot at the event over the weekend!",
+		image: "/assets/projects/treehugger.jpg",
+		video: "/assets/projects/treehugger.mp4",
+		link: "/photos/treehugger"
+	}
+];
--- src/routes/photos/+page.svelte
+++ src/routes/photos/+page.svelte
@@ -1,11 +1,31 @@
 <script>
 	import Carousel from "./Carousel.svelte";
-	import PhotoHero from "../treehugger/PhotoHero.svelte";
+	import PhotoHero from "$lib/components/PhotoHero.svelte";
+	import { events } from "$lib/data/events.js";
+	import Projects from "$lib/components/Projects.svelte";
+	import Title from "$lib/components/Title.svelte";
 </script>
 
 <svelte:head>
 	<title>Photos - Jacob Druery // Creative Developer</title>
+	<link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
 </svelte:head>
 
 <PhotoHero subtitle="// Sometimes I take photos" />
 <Carousel />
+<section>
+	<Title innerText="Recent Events" id="events" />
+	<Projects array={events} />
+</section>
+
+<style>
+	section {
+		padding: 0 1rem 2rem 1rem;
+	}
+
+	@media screen and (min-width: 768px) {
+		section {
+			padding: 0 2rem 2rem 2rem;
+		}
+	}
+</style>
