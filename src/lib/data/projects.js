3ea7dbc79d02163f34d326f798adc75276ec50fe
Jacob Druery <95809005+PullRequestTimeout@users.noreply.github.com>
2023-10-14 19:40:56 -0700

    :truck: Changed assets urls

--- src/lib/data/projects.js
+++ src/lib/data/projects.js
@@ -1,22 +1,12 @@
 export const projects = [
-	// {
-	//     title: "DirtSurfer",
-	//     subtitle: "React / Node.js App",
-	//     date: "2023",
-	//     description:
-	//         "Dirt Surfer is a forecasting webapp for moutainbike trail conditions initially focusing on Rossland BC and surrounding area, and then expanding to other regions over time. Frontend built using React with Material UI, backend API built using Express and MongoDB. ",
-	//     image: "/assets/dirtsurfer.jpg", //Dummy data
-	//     video: "/assets/gooeystartpagevideo.mp4", //Dummy data
-	//     link: "https://dirtsurfer.app",
-	// },
 	{
 		title: "Rooted In Motion",
 		subtitle: "Website - Healthcare",
 		date: "2023",
 		description:
 			"A landing page website for a local manual therapy/osteopathy business located in Rossland, BC. Work includes photograpy/editing, development and deployment. Web Design in collaboration with Sean Makura.",
-		image: "/assets/rootedinmotion.jpg",
-		video: "/assets/rootedinmotionvideo.mp4",
+		image: "/assets/projects/rootedinmotion.jpg",
+		video: "/assets/projects/rootedinmotionvideo.mp4",
 		link: "https://rootedinmotion.ca"
 	},
 	{
@@ -25,8 +15,8 @@ export const projects = [
 		date: "2023",
 		description:
 			"Gooey (GUI) is a startpage to replace the browser's default new tab in the form of a Firefox-Addon. Using vanilla JS, HTML, and CSS as a project to strengthen fundamentals, utilise an external API, and build a client-side CRUD interface from scratch. Drawing from /r/startpages for some inspiration.",
-		image: "/assets/gooeystartpage.jpg",
-		video: "/assets/gooeystartpagevideo.mp4",
+		image: "/assets/projects/gooeystartpage.jpg",
+		video: "/assets/projects/gooeystartpagevideo.mp4",
 		link: "https://addons.mozilla.org/en-CA/firefox/addon/gooey-startpage/" //Needs replacing when Chrome version is published
 	},
 	{
@@ -35,9 +25,9 @@ export const projects = [
 		date: "2023",
 		description:
 			"That's right, this website! I rebuilt my personal portfolio website as a chance to play with Svelte, and ultimately migrated it to SvelteKit as the functionality of the site grew. Designed in collaboration with Sean Makura again after the great work he did on Rooted In Motion.",
-		image: "/assets/jacobeli.jpg",
-		video: "/assets/jacobelivideo.mp4",
-		link: "https://jacobeli.dev"
+		image: "/assets/projects/jacobeli.jpg",
+		video: "/assets/projects/jacobelivideo.mp4",
+		link: "/"
 	}
 ];
 
