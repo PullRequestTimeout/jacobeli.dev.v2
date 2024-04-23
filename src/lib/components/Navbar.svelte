70030c35e4df74a805c48bfdcf0e27d9d1a6c19b
Jacob Druery <95809005+PullRequestTimeout@users.noreply.github.com>
2024-04-22 20:06:57 -0700

    :recycle: Rebuilt nav to fix visual glitch

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -1,53 +1,41 @@
 <script>
 	import { page } from "$app/stores";
-	import { fly } from "svelte/transition";
 
-	let checked = false;
-	const uncheck = () => {
-		checked = false;
-	};
-
-	let screenWidth;
+	let open = false;
+	const closeNav = () => (open = false);
 </script>
-
-<svelte:window bind:innerWidth={screenWidth} />
 
 <header class="mulish">
 	<a href="/">JACOBELI.DEV</a>
-	{#if screenWidth < 768}
-		<input type="checkbox" id="navCheckbox" bind:checked />
-		<label for="navCheckbox" />
-
-		{#if checked}
-			<nav transition:fly={{ x: "100%", duration: 500 }}>
-				<ul>
-					{#if $page.url.pathname === "/"}
-						<li><a on:click={uncheck} href="#work">WORK</a></li>
-						<li><a on:click={uncheck} href="#about">ABOUT</a></li>
-						<li><a on:click={uncheck} href="#contact">CONTACT</a></li>
-					{:else}
-						<li><a on:click={uncheck} href="/">HOME</a></li>
-						<li><a on:click={uncheck} href="/photos">PHOTOS</a></li>
-						<li><a on:click={uncheck} href="#contact">CONTACT</a></li>
-					{/if}
-				</ul>
-			</nav>
-		{/if}
-	{:else}
-		<nav>
-			<ul>
-				{#if $page.url.pathname === "/"}
-					<li><a href="#work">WORK</a></li>
-					<li><a href="#about">ABOUT</a></li>
-					<li><a href="#contact">CONTACT</a></li>
-				{:else}
-					<li><a href="/">HOME</a></li>
-					<li><a href="/photos">PHOTOS</a></li>
-					<li><a href="#contact">CONTACT</a></li>
-				{/if}
-			</ul>
-		</nav>
-	{/if}
+	<button on:click={() => (open = !open)} aria-label="hamburger menu"
+		><div class="hamburger" class:open /></button
+	>
+	<nav class:open class="small-nav">
+		<ul>
+			{#if $page.url.pathname === "/"}
+				<li><a on:click={closeNav} href="#work">WORK</a></li>
+				<li><a on:click={closeNav} href="#about">ABOUT</a></li>
+				<li><a on:click={closeNav} href="#contact">CONTACT</a></li>
+			{:else}
+				<li><a on:click={closeNav} href="/">HOME</a></li>
+				<li><a on:click={closeNav} href="/photos">PHOTOS</a></li>
+				<li><a on:click={closeNav} href="#contact">CONTACT</a></li>
+			{/if}
+		</ul>
+	</nav>
+	<nav class="large-nav">
+		<ul>
+			{#if $page.url.pathname === "/"}
+				<li><a href="#work">WORK</a></li>
+				<li><a href="#about">ABOUT</a></li>
+				<li><a href="#contact">CONTACT</a></li>
+			{:else}
+				<li><a href="/">HOME</a></li>
+				<li><a href="/photos">PHOTOS</a></li>
+				<li><a href="#contact">CONTACT</a></li>
+			{/if}
+		</ul>
+	</nav>
 </header>
 
 <style>
@@ -65,6 +53,22 @@
 		font-weight: 100;
 	}
 
+	nav {
+		position: absolute;
+		display: flex;
+		place-items: center;
+		right: 0;
+		background-color: var(--clr-white);
+		transition: transform 0.4s ease-in-out;
+		height: 4rem;
+		width: 100vw;
+		transform: translateX(100%);
+	}
+
+	nav.open {
+		transform: translateX(0);
+	}
+
 	nav ul {
 		display: flex;
 		gap: 1rem;
@@ -72,102 +76,93 @@
 	}
 
 	nav ul li a {
-		position: relative;
 		color: var(--clr-black);
 	}
 
-	nav ul li a::after {
-		content: "";
-		position: absolute;
-		z-index: -1;
-		bottom: 45%;
-		left: 0;
-		width: 0;
-		height: 2px;
-		background-color: var(--clr-black);
-		transition: width 0.2s ease-in-out;
-	}
-
-	nav ul li a:hover::after,
-	nav ul li a:active::after {
-		width: 100%;
-	}
-
-	input[type="checkbox"] {
-		display: none;
-	}
-
-	label {
+	header button {
 		position: relative;
-		z-index: 10;
-		height: 2rem;
-		aspect-ratio: 1;
+		z-index: 100;
 		background-color: transparent;
+		border: none;
+		cursor: pointer;
+		height: 2rem;
+		width: 2rem;
+		padding: 0;
 	}
 
-	label::before,
-	label::after {
+	div.hamburger {
+		position: relative;
+	}
+
+	div.hamburger::before,
+	div.hamburger::after {
+		transition: 0.4s ease-in-out;
 		content: "";
 		position: absolute;
-		top: 0;
 		left: 0;
-		height: 2px;
 		width: 2rem;
+		height: 2px;
 		background-color: var(--clr-white);
-		transition: transform 0.2s ease-in-out;
 	}
 
-	label::before {
-		top: 35%;
+	div.hamburger::before {
+		translate: 0 0.3rem;
 	}
 
-	label::after {
-		top: 65%;
+	div.hamburger::after {
+		translate: 0 -0.3rem;
 	}
 
-	input[type="checkbox"] ~ label::after,
-	input[type="checkbox"] ~ label::before {
-		transition: 0.4s ease-in-out;
-	}
-
-	input[type="checkbox"]:checked ~ label::before,
-	input[type="checkbox"]:checked ~ label::after {
+	div.hamburger.open::after,
+	div.hamburger.open::before {
 		background-color: var(--clr-black);
-		transform-origin: center;
-		top: 50%;
 	}
 
-	input[type="checkbox"]:checked ~ label::before {
-		transform: rotate(45deg);
+	div.hamburger.open::before {
+		rotate: 45deg;
+		translate: 0 0;
 	}
 
-	input[type="checkbox"]:checked ~ label::after {
-		transform: rotate(-45deg);
+	div.hamburger.open::after {
+		rotate: -45deg;
+		translate: 0 0;
 	}
 
-	nav {
-		position: absolute;
-		display: flex;
-		place-items: center;
-		right: 0;
-		background-color: var(--clr-white);
-		/* transform: translateX(100%); */
-		transition: transform 0.4s ease-in-out;
-		height: 4rem;
-		width: 100vw;
-	}
+	@media (hover: hover) {
+		nav ul li a {
+			position: relative;
+		}
 
-	/* input[type="checkbox"]:checked ~ nav {
-		transform: translateX(0);
-	} */
+		nav ul li a::after {
+			content: "";
+			position: absolute;
+			z-index: -1;
+			bottom: 45%;
+			left: 0;
+			width: 0;
+			height: 2px;
+			background-color: var(--clr-black);
+			transition: width 0.2s ease-in-out;
+		}
+
+		nav ul li a:hover::after,
+		nav ul li a:active::after {
+			width: 100%;
+		}
+	}
 
 	@media screen and (min-width: 768px) {
 		header {
 			padding: 2rem;
 		}
 
-		label {
+		header button,
+		nav.small-nav {
 			display: none;
+		}
+
+		nav.large-nav {
+			display: flex;
 		}
 
 		nav {
